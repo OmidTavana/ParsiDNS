10cd165cfd2805e046b44ad0f764d6e11dfc2489
Omid Tavana <101655734+OmidTavana@users.noreply.github.com>
2024-05-12 20:23:42 +0330

    CreateWinUiProject

---
+++ PopularDNS/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PopularDNS
+{
+	public	 class MainViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		void OnPropertyChanged([CallerMemberName] string name = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		public ICommand ShecanCommand { get; }
+		public ICommand ElektroCommand { get; }
+		public ICommand E403Command { get; }
+		public ICommand BegzarCommand { get; }
+
+	}
+}
